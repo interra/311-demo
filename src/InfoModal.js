bed9f350cd0aaa30f457fcf1e9098f1482b9f326
root <starsinmypockets@gmail.com>
2017-12-19 18:27:09 +0000

    Add copy to info page. Checkpoint.

--- src/InfoModal.js
+++ src/InfoModal.js
@@ -17,7 +17,8 @@ export default class InfoModal extends Component {
           <div className="row justify-content-center pb-5">
             <div className="col-12 text-center">
               <h1>Open Data Visualization</h1>
-              <p class="text-h3">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
+              <p class="text-h3">
+                Welcome to the future - data is everywhere, the digital divide recedes before our very eyes, and we stand on the brink of.... confusion? Given the power of accessible open source technologies, we think that we can do better! We've wired this demo to give just a taste of what can be done with open data and open source technologies on the web.</p>
             </div>
           </div>
 
@@ -29,8 +30,8 @@ export default class InfoModal extends Component {
 
             <div className="col-12 col-md-5 m-md-auto">
               <h2><strong>Philly Open 311</strong></h2>
-              <p class="text-h3">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
-              <p><a href="https://www.froala.com">Learn More &gt;</a></p>
+              <p class="text-h3">Open standards are on the rise. Cities, organizations, and governments are quickly adopting them as they realize that it allows them to leverage powerful existing tools. Philly's Open311 project, utilizing the Open 311 standard, is just an example.</p>
+              <p><a href="http://www.phila.gov/311">Learn More &gt;</a></p>
             </div>
           </div>
         </div>
@@ -43,8 +44,8 @@ export default class InfoModal extends Component {
 
             <div className="col-12 col-md-5">
               <h2><strong>Geospatial Data</strong></h2>
-              <p className="text-h3">Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem Ipsum decided to leave for the far World of Grammar.</p>
-              <p><a href="https://www.froala.com">Learn More &gt;</a></p>
+              <p className="text-h3">Geospatial data is (literally) everywhere. Data is not just in rows and columns anymore: it is on streets and corners, and it follows us around everywhere we go. What does this data tell us about our lives, our cities, our societies? Let's find out!</p>
+              <p><a href="http://postgis.net/">Learn More &gt;</a></p>
             </div>
           </div>
       </div>
@@ -57,8 +58,8 @@ export default class InfoModal extends Component {
 
             <div className="col-12 col-md-5 m-md-auto">
               <h2><strong>GraphQL</strong></h2>
-              <p className="text-h3">On her way she met a copy. The copy warned the Little Blind Text, that where it came from it would have been rewritten a thousand times and everything that was left from its origin would be the word "and" and the Little Blind Text should turn around.</p>
-              <p><a href="https://www.froala.com">Learn More &gt;</a></p>
+              <p className="text-h3">Technology is getting better - APIs are getting smarter, and the way that we can share and access data is improving. GraphQL is the latest and greatest, and makes accessing complex data into a sensible task.</p>
+              <a href="http://graphql.org/">Learn More &gt;</a>
             </div>
          </div> 
         </div>
@@ -71,11 +72,19 @@ export default class InfoModal extends Component {
 
             <div className="col-12 col-md-5 m-md-auto">
               <h2><strong>React</strong></h2>
-              <p className="text-h3">On her way she met a copy. The copy warned the Little Blind Text, that where it came from it would have been rewritten a thousand times and everything that was left from its origin would be the word "and" and the Little Blind Text should turn around.</p>
-              <p><a href="https://www.froala.com">Learn More &gt;</a></p>
+              <p className="text-h3">We utilize a suite of front-end tools to bring intuitive user experience to everyday people. Understanding data shouldn't feel like doing taxes, nor should it require a PHD. Information wants to be free and we have the tools to do it.</p>
+              <a href="https://reactjs.org/">Learn More &gt;</a>
             </div>
           </div>
         </div>
+
+        <div className="row">
+          <div className="col-12 col-md-5 m-md-auto">
+            <p className="text-h3">If you want more info about this project, the tools that we are using, or if you are interested to discuss a data visualization project, get in touch!</p>
+          <button mailto="pjwalker76@gmail.com">CONTACT</button>
+          </div>
+        </div>
+       
        </div>
       </section>		
 		</Modal>

74ad25b10ed06d92082301b70007630183b80634
root <starsinmypockets@gmail.com>
2017-12-19 18:07:57 +0000

    Better marketing page - needs cop

--- src/InfoModal.js
+++ src/InfoModal.js
@@ -1,39 +1,84 @@
 import React, { Component } from 'react'
 import Modal from 'react-modal'
+import gqlLogo from './images/logos/graphql-logo.svg'
+import reactLogo from './images/logos/react-logo.png'
+import phillyLogo from './images/logos/philly-bell-logo.png'
+import nodeLogo from './images/logos/node-logo.png'
+import postGISLogo from './images/logos/postgis-logo.png'
 
 export default class InfoModal extends Component {
   render() {
+    const toggleInfoWindow = this.props.toggleInfoWindow
     return (
     <Modal isOpen={this.props.infoWindowOpen}>
-        <div className="info-window-content">
-					<h1>Welcome to the 311 Dash Demo</h1>
-				  <p class="close-modal doHover" onClick={this.props.toggleInfoWindow}>X</p>
-          <div class="info-window-content-inner">
-					<p>Welcome to the Philly 311 dashboard demo! Click around and feel free to leave feedback at our website: `https://interra.io`!</p>
-          <h3>About this tools</h3>
-          <p>This tool is built with our own stack utilizing a suite of current open source technologies:</p>
-          <ul>
-            <li><a href="#">React</a></li>
-            <li><a href="#">Graphql</a></li>
-            <li><a href="#">PostGis</a></li>
-            <li><a href="#">Leaflet.js</a></li>
-            <li><a href="#">Open Street Map</a></li>
-          </ul>
-          <p>This project also inherits from the <a href="#">react-dash</a> project. We hope to continue to develop this toolset in such a way that it is valuable to municipalities, citizens, members of the open source and open data communities, and developers leveraging open source technolgies.</p>
-          <h3>About Open 311</h3>
-          <p>We are using sample data from the Philly 311 API, which leverages the Open311 standard (link). This means that we can easily implement this dashboard for ANY city or municipality that leverages this standard.</p>
-          <h3>Get in touch!</h3>
-          <p>If you are interested in Open311, or open data generally,  have a look at these references:</p>
-          <ul class="info-list">
-            <li><a href="http://interria.io">Interra Open Data Catalog</a></li>
-            <li><a href="http://wiki.open311.org/" target="_blank">Open 311 WIKI</a></li>
-            <li><a href="https://www.opendataphilly.org/" target="_blank">Open Data Philly</a></li>
-          </ul>
-          <p>If you are interested in contributing or would like to discuss the project or you open data needs, please contact us:</p>
-          <button mailto="pjwalker76@gmail.com">Contact Interra</button>
+      <section className="fdb-block">
+        <span className="close-modal doHover" onClick={toggleInfoWindow}>X</span>
+        <div className="container">
+          <div className="row justify-content-center pb-5">
+            <div className="col-12 text-center">
+              <h1>Open Data Visualization</h1>
+              <p class="text-h3">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
+            </div>
+          </div>
+
+        <div className="info-row">
+          <div className="row text-left align-items-center pt-5 pb-md-5">
+            <div className="col-4 col-md-5">
+              <img alt="image" className="img-fluid" src={phillyLogo}/>
+            </div>
+
+            <div className="col-12 col-md-5 m-md-auto">
+              <h2><strong>Philly Open 311</strong></h2>
+              <p class="text-h3">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
+              <p><a href="https://www.froala.com">Learn More &gt;</a></p>
+            </div>
           </div>
         </div>
-			</Modal>
+
+        <div className="info-row">
+          <div className="row text-left align-items-center pt-5 pb-md-5">
+            <div className="col-4 col-md-5 m-md-auto order-md-5">
+              <img alt="image" className="img-fluid indent" src={postGISLogo}/>
+            </div>
+
+            <div className="col-12 col-md-5">
+              <h2><strong>Geospatial Data</strong></h2>
+              <p className="text-h3">Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem Ipsum decided to leave for the far World of Grammar.</p>
+              <p><a href="https://www.froala.com">Learn More &gt;</a></p>
+            </div>
+          </div>
+      </div>
+
+        <div className="info-row">
+          <div className="row text-left align-items-center pt-5">
+            <div className="col-4 col-md-5">
+              <img alt="image" className="img-fluid indent" src={gqlLogo}/>
+            </div>
+
+            <div className="col-12 col-md-5 m-md-auto">
+              <h2><strong>GraphQL</strong></h2>
+              <p className="text-h3">On her way she met a copy. The copy warned the Little Blind Text, that where it came from it would have been rewritten a thousand times and everything that was left from its origin would be the word "and" and the Little Blind Text should turn around.</p>
+              <p><a href="https://www.froala.com">Learn More &gt;</a></p>
+            </div>
+         </div> 
+        </div>
+          
+        <div className="info-row">
+          <div className="row text-left align-items-center pt-5">
+            <div className="col-4 col-md-5">
+              <img alt="image" className="img-fluid indent" src={reactLogo}/>
+            </div>
+
+            <div className="col-12 col-md-5 m-md-auto">
+              <h2><strong>React</strong></h2>
+              <p className="text-h3">On her way she met a copy. The copy warned the Little Blind Text, that where it came from it would have been rewritten a thousand times and everything that was left from its origin would be the word "and" and the Little Blind Text should turn around.</p>
+              <p><a href="https://www.froala.com">Learn More &gt;</a></p>
+            </div>
+          </div>
+        </div>
+       </div>
+      </section>		
+		</Modal>
       )
     }
 }

d6372ae4f01772cfeba416d2322263660fcc0419
root <starsinmypockets@gmail.com>
2017-12-29 16:14:31 +0000

    Pass ENV vars for api path

--- src/index.js
+++ src/index.js
@@ -11,11 +11,14 @@ import { ApolloProvider } from 'react-apollo'
 import {ApolloClient} from 'apollo-client'
 
 // should prob use ENV
-const API_URI = "http://45.79.171.179:3333/graphql/"
+//const API_URI = "http://45.79.171.179:3333/graphql/"
+const API_URL= process.env.REACT_APP_API_URL_DEV
 const client = new ApolloClient({
-  link: new HttpLink({uri: API_URI}),
+  link: new HttpLink({uri: API_URL}),
   cache: new InMemoryCache()
 })
+
+console.log('Interra 311 Demo', API_URL, process.env)
 
 ReactDOM.render((
   <ApolloProvider client={client}>
